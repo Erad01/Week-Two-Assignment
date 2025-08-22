87d2b9b13daca33e88218dc91ddf2934fcc6a066
DARE VICTOR OTITOJU <otitojudare@gmail.com>
2025-08-22 17:19:14 +0100

    feat: some element of the thumbnail added

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ console.log("Hello World")
 
 const images = [
   {
-    url: "./Asset folder/alliance-5201816_1280.jpg", //relative path for local images OR link to the image
+    url: "./assetFolder/alianceArena.jpg", //relative path for local images OR link to the image
     altText: "Picture of Bayern Stadium",
+    className: "img1",
   },
   {
-    url: "./Asset folder/anfield-1275557_1280.jpg",
+    url: "./assetFolder/anfield.jpg",
     altText: "Picture of Liverpool Stadium",
+    className: "img2",
   },
   {
-    url: "./Asset folder/stadium-537926_1280.jpg",
+    url: "./assetFolder/benabeu.jpg",
     altText: "Picture of Real Madrid Stadium",
+    className: "img3",
   },
 ];
-
+console.log(images)
 //TODO: I need to create my thumbnail images
 // function createThumbnails(){
 // select the DOM element (thumbnail-container) to contain our thumbnails
 //this is a repetitive task --> loop through our array (using the length property)
+
 //Inside our loop we need to do this:
 // - create img element
 // - update the src and alt attributes of the img element to match those in the array (parameters)
@@ -29,6 +33,22 @@ const images = [
 // - add an event listener to each image --> the event handler of this listener is the function you write to create large images
 // - append the created images to the thumbnail-container
 // }
+function createThumbnails (images){
+    const thumbnail = document.getElementById("thumbnail-container");
+
+    for (let i = 0; i < images.length; i++){
+    const thumbnailImage = document.createElement("img");
+    thumbnailImage.src = images[i].url;
+    thumbnailImage.alt = images[i].altText;
+    // console.log(thumbnailImage.src)
+    // console.log(thumbnailImage.altText)
+    thumbnailImage.className = images[i].className;
+    thumbnailImage.addEventListener("click", createLargeImagesHandler);
+    thumbnail.appendChild(thumbnailImage)
+}
+}
+createThumbnails(images)
+// Remeember to call the function
 
 //TODO: I need to create my large images
 //this task is the event handler for the thumbnail events
